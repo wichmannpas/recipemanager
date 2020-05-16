3d587d9b93c1047b42d6a38b66a31b81df286953
Pascal Wichmann <git@pa-w.de>
2020-05-16 09:43:06 +0200

    Basic recipe management through Django admin

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.db.models import Prefetch
 
-# Register your models here.
+from recipe.models import Ingredient, Recipe, RecipeIngredient, RecipeInstance, Tag
+
+admin.site.register(Tag)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeInstanceInline(admin.TabularInline):
+    model = RecipeInstance
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'tag_str',
+    )
+    list_filter = (
+        'tags',
+    )
+    search_fields = (
+        'name',
+    )
+
+    inlines = (
+        RecipeIngredientInline,
+        RecipeInstanceInline,
+    )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related(Prefetch('tags', Tag.objects.order_by('name')))
--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,55 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        'Recipe', on_delete=models.CASCADE, related_name='recipe_ingredients')
+    ingredient = models.ForeignKey(
+        Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredients')
+
+    amount = models.DecimalField(max_digits=10, decimal_places=3)
+    unit = models.CharField(max_length=10, default='g', choices=(
+        ('g', 'g'),
+        ('ml', 'ml'),
+        ('l', 'l'),
+        ('tl', 'TL'),
+        ('el', 'EL'),
+    ))
+    note = models.TextField(null=True, blank=True)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=30)
+    tags = models.ManyToManyField(Tag, related_name='recipes')
+    ingredients = models.ManyToManyField(Ingredient, through=RecipeIngredient)
+
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def tag_str(self) -> str:
+        return ', '.join(tag.name for tag in self.tags.all())
+
+
+class RecipeInstance(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='instances')
+    day = models.DateField(default=date.today)
+    notes = models.TextField(null=True, blank=True)
